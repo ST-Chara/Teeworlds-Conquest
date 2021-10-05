7b6e724ca568f73eef94949b8ad1654844ade216
ST-Chara <74450074+ST-Chara@users.noreply.github.com>
2021-10-05 19:11:13 +0800

    Update version.h

--- src/game/version.h
+++ src/game/version.h
@@ -2,9 +2,12 @@
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
-#include <generated/nethash.cpp>
-#define GAME_VERSION "0.7.1"
-#define GAME_NETVERSION "0.7 802f1be60a05665f"
-#define CLIENT_VERSION 0x0701
-static const char GAME_RELEASE_VERSION[8] = {'0', '.', '7', '.', '1', 0};
+#ifndef GAME_RELEASE_VERSION
+#define GAME_RELEASE_VERSION "15.4"
+#endif
+#define GAME_VERSION "0.6.4, " GAME_RELEASE_VERSION
+#define GAME_NETVERSION "0.6 626fce9a778df4d4"
+#define GAME_NAME "DDNet"
+#define CLIENT_VERSIONNR 15040
+extern const char *GIT_SHORTREV_HASH;
 #endif
